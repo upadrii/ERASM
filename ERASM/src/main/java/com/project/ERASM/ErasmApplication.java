b3890f72e9b099ee1312a8572feae77f5685f179
Karthik upadri <Karthikupadri@gmail.com>
2026-06-29 21:46:09 +0530

    fix: ignore Eclipse and Maven generated files

---
+++ ERASM/src/main/java/com/project/ERASM/ErasmApplication.java
@@ -0,0 +1,13 @@
+package com.project.ERASM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErasmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErasmApplication.class, args);
+	}
+
+}
---
+++ ERASM/src/test/java/com/project/ERASM/ErasmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.ERASM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErasmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
